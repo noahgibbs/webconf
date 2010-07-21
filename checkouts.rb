a1d2ee72611f1f22c851191d13ee5e4a75f8904f
WWW user <www@ubuntu.(none)>
2010-07-20 19:00:31 -0700

    Update checkouts script

--- checkouts.rb
+++ checkouts.rb
@@ -7,9 +7,17 @@ repos = {
   :wantmyjob => nil,
   :shannasdaddy => nil,#'ssh://angelbob@arch.angelbob.com/home/angelbob/src/master/shannasdaddy.git',
   :webconf => 'git@github.com:noahgibbs/webconf.git',
+  "cheaptoad-catcher".to_sym => 'git@github.com:noahgibbs/cheaptoad-catcher.git',
 }
 
-WebRoot = File.join(File.dirname(__FILE__), "..")
+WebRoot = File.join(File.expand_path(File.dirname(__FILE__)), "..")
+
+repos.each do |repo_name, repo_path|
+  unless File.exists? File.join(WebRoot, repo_name.to_s)
+    Dir.chdir WebRoot
+    system "git clone #{repo_path}"
+  end
+end
 
 # refresh all repositories
 repos.each do |repo_name, repo_path|
