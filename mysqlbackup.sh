7ab1afbac9b9b2e13c620637b58dfe0a426835ed
WWW user <www@ubuntu.(none)>
2010-12-12 15:28:17 -0800

    MySQL backup

--- mysqlbackup.sh
+++ mysqlbackup.sh
@@ -1,1 +1,3 @@
-mysqldump --lock-tables --add-drop-database --all-databases --add-locks --create-options --comments --dump-date --flush-logs --password=$MYSQL_PASSWORD --user=$MYSQL_USER
+DATE=`date +%Y%b%d`
+mysqldump --lock-tables --add-drop-database --all-databases --add-locks --create-options --flush-logs --password=$MYSQL_PASSWORD --user=$MYSQL_USER > ~/backups/mysql_$DATE
+gzip -9 ~/backups/mysql_$DATE
