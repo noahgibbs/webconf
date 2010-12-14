d19acf636d1b06a8443a2daad5cb7e0a56256703
WWW user <www@ubuntu.(none)>
2010-12-14 15:49:34 -0800

    Fix (?) mysqlbackup.sh for use by cron

--- mysqlbackup.sh
+++ mysqlbackup.sh
@@ -1,3 +1,4 @@
 DATE=`date +%Y%b%d`
+source ~/.bashrc  # MYSQL_USER and MYSQL_PASSWORD
 mysqldump --lock-tables --add-drop-database --all-databases --add-locks --create-options --flush-logs --password=$MYSQL_PASSWORD --user=$MYSQL_USER > ~/backups/mysql_$DATE
 gzip -9 ~/backups/mysql_$DATE
